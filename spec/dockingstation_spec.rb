f32fda505bc32df380de13a6633a53c2c33bd895
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-05-26 17:57:35 +0100

    Added error raising

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,9 +3,16 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike.working?).to eq(true)
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
   
   it 'docks something' do
