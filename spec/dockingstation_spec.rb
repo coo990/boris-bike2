ca4bad5da60a9288e1f8f4ce164064596a35c015
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-05-27 16:24:29 +0100

    Added fail error to dock method

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -14,15 +14,22 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
-  
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-  
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+
+  describe '#dock' do
+    it 'error if docking station is full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Full'
+    end
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
   end
 end
